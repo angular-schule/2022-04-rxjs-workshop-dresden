e592080e0f4626657f36905466c2eefe4e206638
Ferdinand Malcher <ferdinand@malcher.media>
2022-04-05 12:10:13 +0200

    Observables erstellen

--- src/app/exercises/creating/creating.component.ts
+++ src/app/exercises/creating/creating.component.ts
@@ -23,6 +23,63 @@ export class CreatingComponent {
     /******************************/
 
     
+    const myFancyObs$ = new Observable<string>(sub => {
+      sub.next('A');
+      sub.next('B');
+      sub.next('C');
+      sub.complete();
+    });
+
+    // of('A', 'B', 'C')
+    // from([1,2,3,4,5])
+    // interval(1000)
+    // timer(3000)
+
+    timer(3000, 500).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE')
+    });
+
+
+    /******************************/
+
+    function producer(sub: any) {
+      sub.next(Math.random());
+      sub.next(5);
+
+      setTimeout(() => {
+        sub.next(10);
+      }, 2000)
+
+      const myInterval = setInterval(() => {
+        const result = Math.random();
+        sub.next(result);
+        console.log('INNER', result);
+      }, 1000)
+
+      // Teardown Logic
+      return () => {
+        console.log('Teardown');
+        clearInterval(myInterval);
+      }
+    }
+
+
+    const obs = {
+      next: (e: any) => console.log(e),
+      // error: (err: any) => console.error(err),
+      // complete: () => console.log('COMPLETE'),
+    }
+
+    const myObs$ = new Observable(producer);
+    // const subscription = myObs$.subscribe(obs);
+
+
+    /*setTimeout(() => {
+      subscription.unsubscribe();
+    }, 10000)*/
+
+
     /******************************/
   }
 
