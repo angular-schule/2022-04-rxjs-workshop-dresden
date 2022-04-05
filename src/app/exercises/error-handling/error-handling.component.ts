a70c1c5c6836e3cab9063f92bd1eda83f05ed9fc
Ferdinand Malcher <ferdinand@malcher.media>
2022-04-05 15:11:38 +0200

    Error Handling

--- src/app/exercises/error-handling/error-handling.component.ts
+++ src/app/exercises/error-handling/error-handling.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReplaySubject, throwError, of, EMPTY, retry, catchError } from 'rxjs';
+import { ReplaySubject, throwError, of, EMPTY, retry, catchError, Subject, Observable, NEVER } from 'rxjs';
 
 import { ExerciseService } from '../exercise.service';
 
@@ -24,7 +24,24 @@ export class ErrorHandlingComponent {
 
   start() {
     this.es.randomError().pipe(
-      retry(5)
+      // retry(5),
+      catchError(err => {
+        // Fehler weiterwerfen
+        // return new Observable(sub => sub.error('Böser Fehler!'));
+        // return throwError(() => 'Böser Fehler!');
+        // throw 'Noch ein Fehler!'; // empfohlen!
+
+
+        // Fehler ersetzen
+        // return of('Fehler nicht mehr vorhanden!', '😻');
+
+        // Fehler ignorieren
+        // return '';
+        // return [];
+        // return of();
+        return EMPTY; // keine Werte, dann complete
+        // return NEVER; // keine Werte, kein complete
+      })
     ).subscribe({
       next: e => this.logStream$.next(e),
       error: err => this.logStream$.next('❌ ERROR: ' + err)
