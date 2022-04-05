b64d2e666d1e4cb764756bfe4a754cc170fe84a3
Ferdinand Malcher <ferdinand@malcher.media>
2022-04-05 13:34:08 +0200

    scan/reduce

--- src/app/exercises/game-score/game-score.component.ts
+++ src/app/exercises/game-score/game-score.component.ts
@@ -21,7 +21,25 @@ export class GameScoreComponent {
 
     /******************************/
 
-    
+    this.score$.pipe(
+      scan((acc, item) => acc + item, 0)
+    ).subscribe(score => {
+      this.currentScore = score;
+    });
+
+    this.score$.pipe(
+      reduce((acc, item) => acc + item, 0)
+    ).subscribe(score => {
+      this.finalScore = score;
+    });
+
+
+
+    [1,2,3,4,5].reduce((acc, item) => {
+      return acc + item;
+    }); // 15
+
+
     /******************************/
 
     this.score$.subscribe({
